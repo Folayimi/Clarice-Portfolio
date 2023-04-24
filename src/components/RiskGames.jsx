88606d1996d3241609066e4e7b5d6dd4d7771678
Folayimi <ridwanfolayimi@gmail.com>
2023-04-24 19:58:28 +0100

    made some changes

--- src/components/RiskGames.jsx
+++ src/components/RiskGames.jsx
@@ -1,19 +1,19 @@
 import Image from "next/image";
-import steps from "../Assets/steps.png";
 import steps2 from "../Assets/steps2.png";
 import bubble from "../Assets/bubble.png";
+import bubble2 from "../Assets/bubble2.png";
 
 const RiskGames = () => {
   return (
     <>
-      <div className="cflexms pt-32 bg-gradient-to-b from-secondary5-200 to-secondary5-100 text-primary1 md:pt-20">
+      <div className="cflexms pt-32 bg-gradient-to-b h-full from-secondary5-200 to-secondary5-100 text-primary1 md:pt-20">
         <div className="cflexss px-16 md:p-5">
           <div className="flexbm w-full gap-5 md:flex-col md:gap-10">
             <div className="cflexsm w-1/3 gap-5 md:w-2/3">
               <div className="flexmm w-[20em] md:w-[15em]">
                 <Image src={steps2} alt="gogi" width="100%" height="100%" />
-              </div>              
-              <div className="bg-primary1 rounded-3xl py-2 px-5 text-black font-extrabold cursor-pointer hover:text-primary1 hover:bg-black hover:font-extrabold transition-colors duration-300">
+              </div>
+              <div className="bg-primary1 rounded-3xl py-2 px-5 text-black cursor-pointer hover:text-primary1 hover:bg-black transition-colors duration-300">
                 Visit
               </div>
             </div>
@@ -31,9 +31,12 @@ const RiskGames = () => {
               </p>
             </div>
           </div>
-          <div className="flexmm gap-3 pt-28 pb-32 md:text-center w-full">
-            <Image src={bubble} alt="companies" width="100%" height="100%" />
-          </div>
+          <div className="flexmm gap-3 pt-28 pb-32 md:text-center w-full">            
+            <div className="flexmm w-3/4 md:w-full">
+              <Image className="block md:hidden" src={bubble} alt="companies" width="100%" height="100%" />
+              <Image className="hidden md:block" src={bubble2} alt="companies" width="100%" height="100%" />
+            </div>
+          </div>        
         </div>
       </div>
     </>
