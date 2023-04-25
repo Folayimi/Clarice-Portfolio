d4be44382e83d69af9dab5e5b9d8b6e5f2a3344e
ogunlade ridwan <ridwanfolayimi@gmail.com>
2023-04-25 00:04:51 -0700

    made some changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,6 +5,7 @@ import {
   UserCircleOutline,
   PencilOutline,
   PhoneOutline,
+  MenuOutline
 } from "heroicons-react";
 import { useState } from "react";
 import Image from "next/image";
@@ -39,7 +40,7 @@ const Header = () => {
                 router.push("/");
                 setMargin(-100);
               }}
-            >
+            >              
               <HomeOutline size="30px" color="black" />
               Home
             </li>
@@ -148,7 +149,8 @@ const Header = () => {
             setMargin(0);
           }}
         >
-          <svg
+          <MenuOutline color="white" size="35px"/>
+          {/* <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             fill="currentColor"
@@ -159,7 +161,7 @@ const Header = () => {
               d="M3 6.75A.75.75 0 013.75 6h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 6.75zM3 12a.75.75 0 01.75-.75h16.5a.75.75 0 010 1.5H3.75A.75.75 0 013 12zm0 5.25a.75.75 0 01.75-.75H12a.75.75 0 010 1.5H3.75a.75.75 0 01-.75-.75z"
               clipRule="evenodd"
             />
-          </svg>
+          </svg> */}
         </div>
       </div>
     </>
