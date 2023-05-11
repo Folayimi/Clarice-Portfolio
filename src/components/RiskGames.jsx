46f90553da825e67a15d42f890c436fe103c9e4e
Folayimi <ridwanfolayimi@gmail.com>
2023-05-11 20:53:51 +0100

    made some changes

--- src/components/RiskGames.jsx
+++ src/components/RiskGames.jsx
@@ -8,7 +8,7 @@ const RiskGames = () => {
   const router = useRouter();
   return (
     <>
-      <div className="cflexmm pt-32 bg-gradient-to-b w-full h-full from-secondary5-200 to-secondary5-100 text-primary1 md:pt-20">
+      <div className="cflexmm pt-32 bg-gradient-to-b w-full h-[100%] lg:h-[100vh] from-secondary5-200 to-secondary5-100 text-primary1 md:pt-20">
         <div className="cflexsm px-5 md:p-7">
           <div className="flexmm w-full gap-5 md:flex-col pr-[7%] md:pr-0 md:gap-10">
             <div className="cflexsm w-1/3 gap-5 md:w-full">
