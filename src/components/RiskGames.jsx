462fc72259293d8f8c84988210675a773cbefacb
Folayimi <ridwanfolayimi@gmail.com>
2023-05-17 01:38:23 +0100

    made some changes

--- src/components/RiskGames.jsx
+++ src/components/RiskGames.jsx
@@ -11,8 +11,8 @@ const RiskGames = () => {
       <div className="cflexmm pt-32 bg-gradient-to-b w-full h-[100%] lg:h-[100vh] from-secondary5-200 to-secondary5-100 text-primary1 md:pt-20">
         <div className="cflexsm md:px-0">
           <div className="flexmm w-[80%] pt-10 md:w-full gap-5 px-0 md:px-7 md:flex-col pr-7% md:gap-10">
-            <div className="cflexsm gap-2 w-1/3 md:w-full">
-              <div className="flexmm w-[20em] md:w-[15em] sm:w-[70%]">
+            <div className="cflexsm gap-2 sm:gap-5 w-1/3 md:w-full">
+              <div className="flexmm w-[20em] md:w-[8em] sm:w-[70%]">
                 <Image
                   src={"/steps2.svg"}
                   alt="riskgames"
@@ -22,7 +22,7 @@ const RiskGames = () => {
               </div>
               <Link
                 href="https://riskgames.vercel.app"
-                className="bg-primary1 rounded-3xl py-2 px-5 text-black cursor-pointer hover:text-primary1 hover:bg-black transition-colors duration-500"
+                className="bg-primary1 rounded-3xl py-2 px-5 sm:px-10 sm:py-3 text-black sm:font-semibold sm:text-xl cursor-pointer hover:text-primary1 hover:bg-black transition-colors duration-500"
               >
                 Visit
               </Link>
@@ -44,7 +44,7 @@ const RiskGames = () => {
               </p>
             </div>
           </div>
-          <div className="cflexmm pt-28 md:pt-20 pb-32 w-full gap-5">
+          <div className="cflexmm pt-28 md:pt-20 pb-32 w-full gap-10">
             <p className="font-semibold text-4xl md:text-3xl">Games</p>
             <div className="flexmm w-3/4 md:w-full  ">
               <div className="flexmm relative block sm:hidden w-[100%]">
@@ -122,19 +122,20 @@ const RiskGames = () => {
                   width={100}
                   height={50}
                 />
-                <div className="absolute top-0 left-0 w-full h-full cflexsm text-black">
-                  <div className="mt-[-8%] w-[100%] cflexsm">
+                <div className="absolute top-0 left-0 w-screen h-full cflexsm text-black">
+                  <div className="absolute w-full cflexsm">
                     <Image
-                      src={"/deskbubble.svg"}
+                      className="mt-[-15%]"
+                      src={"/mbubble.svg"}
                       alt="smt"
                       width={100}
                       height={100}
                     />
-                    <Link href="" className="mt-[-2em] underline italic text-xl">
+                    <Link href="" className="mt-[-35%] underline italic text-xl">
                       fidgetbubbles.com
                     </Link>
                   </div>
-                  <div className="cflexss py-5 px-12 w-full gap-5 h-full">
+                  <div className=" mt-[55%] cflexss py-5 px-12 w-full gap-5">
                     <div className="w-2/3 flexsm">
                       <Image
                         src={"/psf.svg"}
@@ -143,7 +144,7 @@ const RiskGames = () => {
                         height={100}
                       />
                     </div>
-                    <div className="flex flex-col justify-center items-start gap-3 text-black w-full text-justify">
+                    <div className="flex flex-col justify-center items-start gap-3 text-black text-sm w-full text-justify">
                       <p>Bubble popping has never been this satisfying</p>
                       <p>
                         Fidget Bubbles is designed to help relieve stress and
@@ -157,7 +158,7 @@ const RiskGames = () => {
                         of a higher score.
                       </p>
                     </div>
-                    <div className="cflexmm w-full gap-3">
+                    <div className="cflexmm mt-[-3%] w-full gap-3">
                       <div className="bg-black p-3 rounded-xl w-[60%] cursor-pointer">
                         <Image
                           src={"/playSt.svg"}
